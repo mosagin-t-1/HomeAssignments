01b21f275145c915f0b9cc8641ada5bd845237ef
Mosagin-T-1 <st142155@student.spbu.ru>
2025-10-08 09:22:00 +0000

    sayHello

---
+++ Assignment1/hello.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: Timofey Mosagin
+ * Email: st142155@student.spbu.ru
+ * Project: Assignment 1
+ */
+#include "hello.h"
+#include <iostream>
+
+void myproject::sayHello(const std::string& name) {
+    std::cout << "Hello, " << name << "!" << std::endl;
+}
---
+++ Assignment1/hello.h
@@ -0,0 +1,16 @@
+/*
+ * Author: Timofey Mosagin
+ * Email: st142155@student.spbu.ru
+ * Project: Assignment 1
+ */
+#ifndef HELLO_H
+#define HELLO_H
+
+#include <string>
+
+namespace myproject {
+    void sayHello(const std::string& name);
+}
+
+#endif
+
